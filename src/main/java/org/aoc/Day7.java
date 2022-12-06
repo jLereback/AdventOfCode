cf1f08d39c6f5fcb786d34100453ce5fbcc20a1c
Julia <julia.corell@gmail.com>
2022-12-06 19:23:05 +0100

    Prepare for Day7

---
+++ src/main/java/org/aoc/Day7.java
@@ -0,0 +1,12 @@
+package main.java.org.aoc;
+
+import main.java.org.aoc.tools.FileReader;
+
+public class Day7 {
+	static String input = FileReader.getFileAsString("AoC_Day7.txt");
+
+
+	public static void main(String[] args) {
+		System.out.println(input);
+	}
+}
