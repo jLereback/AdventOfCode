a0f222209229f57b125c29b25198dc3989fa5b95
Julia <julia.corell@gmail.com>
2022-12-07 07:09:58 +0100

    Add test input for Day7

--- src/main/java/org/aoc/Day7.java
+++ src/main/java/org/aoc/Day7.java
@@ -2,11 +2,29 @@ package main.java.org.aoc;
 
 import main.java.org.aoc.tools.FileReader;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Day7 {
 	static String input = FileReader.getFileAsString("AoC_Day7.txt");
 
 
 	public static void main(String[] args) {
-		System.out.println(input);
+
+
+
+
+
+
+
+		//System.out.println(Arrays.toString(test.split("dir")));
+
+		part1();
+	}
+
+	private static void part1() {
+
+
+
 	}
 }
