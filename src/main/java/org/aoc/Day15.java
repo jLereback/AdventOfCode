e3cb518ebc60f86c516426ec93314b647e9bd190
Julia <julia.corell@gmail.com>
2022-12-15 16:56:21 +0100

    Add CreateClassOfToday and remove data

---
+++ src/main/java/org/aoc/Day15.java
@@ -0,0 +1,24 @@
+package main.java.org.aoc;
+
+import main.java.org.aoc.tools.FileReader;
+
+public class Day15 {
+	static String INPUT = FileReader.getFileAsString("15");
+
+	public static void main(String[] args) {
+		System.out.println(INPUT);
+
+		part1();
+		part2();
+	}
+
+	private static void part1() {
+
+	}
+
+	private static void part2() {
+
+	}
+
+
+}
--- src/main/java/org/aoc/tools/CreateClassOfToday.java
+++ src/main/java/org/aoc/tools/CreateClassOfToday.java
@@ -10,24 +10,47 @@ public class CreateClassOfToday {
 	static int day = java.time.LocalDate.now().getDayOfMonth();
 
 	public static void main(String[] args) throws Exception {
-		Path path = Path.of("src/main/java/org/aoc/Day" + day + ".java");
-		createClass(path);
+		Path pathOfClass = Path.of("src/main/java/org/aoc/Day" + day + ".java");
+		Path pathOfData = Path.of("src/main/data/AoC_Day" + day + ".txt");
+		Path pathOfPuzzle = Path.of("src/main/puzzle/puzzle_day" + day + ".md");
+		createClass(pathOfClass);
+		createTxtFile(pathOfData);
+		createMd(pathOfPuzzle);
 	}
 
-	public static void createClass(Path path) {
+	private static void createMd(Path pathOfPuzzle) {
 		try {
-			Files.writeString(path, CLASS, StandardOpenOption.CREATE_NEW);
+			Files.writeString(pathOfPuzzle, MD, StandardOpenOption.CREATE_NEW);
 		} catch (IOException e) {
 			System.out.println(e.getClass().getName() + ": " + e.getMessage());
 		}
 	}
 
+	private static void createTxtFile(Path pathOfData) {
+		try {
+			Files.writeString(pathOfData, "", StandardOpenOption.CREATE_NEW);
+		} catch (IOException e) {
+			System.out.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+	}
+
+	public static void createClass(Path pathOfClass) {
+		try {
+			Files.writeString(pathOfClass, CLASS, StandardOpenOption.CREATE_NEW);
+		} catch (IOException e) {
+			System.out.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+	}
+
+	static String MD = String.format("""
+			#  Day %d:\s
+			""", day);
 	static String CLASS = String.format("""
 			package main.java.org.aoc;
 			   
 			import main.java.org.aoc.tools.FileReader;
 			   
-			public class Day14 {
+			public class Day%d {
 				static String INPUT = FileReader.getFileAsString("%d");
 			   
 				public static void main(String[] args) {
@@ -46,6 +69,6 @@ public class CreateClassOfToday {
 				}
 			
 	
-			}""",day);
+			}""",day, day);
 
 }
